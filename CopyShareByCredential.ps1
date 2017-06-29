9b7f936ef606ed2e36972c05a5e5de0a3f6546c8
George Aleksandriya <giorgi.alexandria@gmail.com>
2017-06-29 21:28:33 +0300

    Add script for copying shared folder.

---
+++ CopyShareByCredential.ps1
@@ -0,0 +1,24 @@
+﻿param
+(
+    [Parameter(Mandatory = $true)]
+    [string]$share,
+
+    [Parameter(Mandatory = $true)]
+    [string]$destination,
+
+    [Parameter(Mandatory = $true)]
+    [string]$userName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$password
+)
+
+try
+{
+    net use $share $password /USER:$userName
+    Copy-Item -Path $share -Destination $destination –Recurse -Force
+}
+finally
+{
+    net use $share /delete /y
+}
