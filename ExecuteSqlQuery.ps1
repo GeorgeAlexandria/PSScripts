956f3c97a21b31728e3d59985bdbecf2b10718e4
George Aleksandriya <giorgi.alexandria@gmail.com>
2017-07-02 00:29:24 +0300

    Add script for sql query execution.

---
+++ ExecuteSqlQuery.ps1
@@ -0,0 +1,28 @@
+﻿param
+(
+    [Parameter(Mandatory = $true)]
+    [string]$databaseName,
+
+    [string]$sqlServerName = "(localdb)\.",
+
+    [Parameter(Mandatory = $true)]
+    [string]$sqlQuery
+)
+
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
+try
+{
+    $smoServer = new-object Microsoft.SqlServer.Management.Smo.Server($sqlServerName)
+    if(!$smoServer.Databases.Contains($databaseName))
+    {
+        Write-Error "Database $databaseName does not exist on $sqlServerName"
+        exit 1
+    }
+    $db = $smoServer.Databases[$databaseName]
+    $db.ExecuteNonQuery($sqlQuery)
+}
+catch
+{
+    Write-Error ($_ | Format-List * -Force | Out-String)
+    exit 1
+}
