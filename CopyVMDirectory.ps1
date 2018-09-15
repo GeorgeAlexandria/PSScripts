910fc832d2aba7459ff9c9935926792549a5dd9b
George Aleksandriya <giorgi.alexandria@gmail.com>
2018-09-15 15:51:49 +0300

    Added script to copy directory to HyperV guest VM.

---
+++ CopyVMDirectory.ps1
@@ -0,0 +1,23 @@
+﻿param
+(
+    [Parameter(Mandatory = $true)]
+    [string]$winName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$source,
+
+    [Parameter(Mandatory = $true)]
+    [string]$destination
+)
+
+function CopyFiles ([string]$sourceFolder, [string]$destinationFolder)
+{
+    Get-ChildItem -Path $sourceFolder -Force  -File | foreach{
+            Write-Output("Copying file: {0}" -f $_.FullName);
+            Copy-VMFile $winName -SourcePath $_.FullName -DestinationPath ("{0}\{1}" -f $destinationFolder, $_.Name) -CreateFullPath -FileSource Host -Force;
+        }
+
+    Get-ChildItem -Path $sourceFolder -Force -Directory | foreach{CopyFiles $_.FullName ("{0}\{1}" -f $destinationFolder, $_.Name)}
+}
+
+CopyFiles $source $destination
